63787803c1bef2e5bb3e425820742577af586b8e
liaowilly12123 <willyliao0425@gmail.com>
2018-04-04 23:03:58 -0600

    made navbar fixed

--- packageManager/src/components/logging/log.vue
+++ packageManager/src/components/logging/log.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <b-nav tabs>
       <b-nav-item to="/testing">Testing</b-nav-item>
       <b-nav-item to="/logging" active>Logging</b-nav-item>
@@ -11,6 +12,107 @@
       <b-nav-item to="/object-relational-mapping">Object Relational Mapping</b-nav-item>
       <b-nav-item to="/xml">XML</b-nav-item>
     </b-nav>
+    <b-card>
+      <b-media no-body>
+        <b-media-aside vertical-align="center">
+          <b-img width="256" height="256" src="http://juliandance.org/wp-content/uploads/2016/01/RedApple.jpg"/>
+        </b-media-aside>
+        <b-media-body class="ml-3">
+          <h5 class="mt-0">Media Title</h5>
+          <p>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
+            sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+            Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
+            in faucibus.
+          </p>
+          <p class="mb-0">
+            Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum
+            sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          </p>
+          <b-media>
+            <h5 class="mt-0">Nested Media</h5>
+            Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue
+            felis in faucibus.
+          </b-media>
+        </b-media-body>
+      </b-media>
+    </b-card>
+    <b-card>
+      <b-media no-body>
+        <b-media-aside vertical-align="center">
+          <b-img width="256" height="256" src="http://juliandance.org/wp-content/uploads/2016/01/RedApple.jpg"/>
+        </b-media-aside>
+        <b-media-body class="ml-3">
+          <h5 class="mt-0">Media Title</h5>
+          <p>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
+            sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+            Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
+            in faucibus.
+          </p>
+          <p class="mb-0">
+            Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum
+            sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          </p>
+          <b-media>
+            <h5 class="mt-0">Nested Media</h5>
+            Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue
+            felis in faucibus.
+          </b-media>
+        </b-media-body>
+      </b-media>
+    </b-card>
+    <b-card>
+      <b-media no-body>
+        <b-media-aside vertical-align="center">
+          <b-img width="256" height="256" src="http://juliandance.org/wp-content/uploads/2016/01/RedApple.jpg"/>
+        </b-media-aside>
+        <b-media-body class="ml-3">
+          <h5 class="mt-0">Media Title</h5>
+          <p>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
+            sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+            Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
+            in faucibus.
+          </p>
+          <p class="mb-0">
+            Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum
+            sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          </p>
+          <b-media>
+            <h5 class="mt-0">Nested Media</h5>
+            Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue
+            felis in faucibus.
+          </b-media>
+        </b-media-body>
+      </b-media>
+    </b-card>
+    <b-card>
+      <b-media no-body>
+        <b-media-aside vertical-align="center">
+          <b-img width="256" height="256" src="http://juliandance.org/wp-content/uploads/2016/01/RedApple.jpg"/>
+        </b-media-aside>
+        <b-media-body class="ml-3">
+          <h5 class="mt-0">Media Title</h5>
+          <p>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
+            sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+            Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis
+            in faucibus.
+          </p>
+          <p class="mb-0">
+            Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum
+            sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          </p>
+          <b-media>
+            <h5 class="mt-0">Nested Media</h5>
+            Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue
+            felis in faucibus.
+          </b-media>
+        </b-media-body>
+      </b-media>
+    </b-card>
+  </div>
 </template>
 
 <script>

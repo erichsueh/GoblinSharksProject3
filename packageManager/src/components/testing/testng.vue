dc2c77a3ffb010b8bbd12d41803514378c7c70b3
vuk@ualberta.ca <vuk@ualberta.ca>
2018-04-05 00:30:33 -0600

    images inside cards in test1 example

--- packageManager/src/components/testing/testng.vue
+++ packageManager/src/components/testing/testng.vue
@@ -1,9 +1,9 @@
 <template>
-  <b-card title="junit4"
+  <b-card title="testng"
             tag="article"
             style="max-width: 50rem;"
             class="mb-2">
-    <img src="../../assets/Screen_Shot_2018-04-04_at_7.43.04_PM.png"/>        
+    <img src="../../assets/Screen_Shot_2018-04-04_at_7.43.04_PM.png"/>
     <p class="card-text">
       Some quick example text to build on the card title and make up the bulk of the card's content.
     </p>
@@ -13,7 +13,7 @@
 
 <script>
 export default {
-  name: 'test4'
+  name: 'testng'
 }
 </script>
 

8e9b435d76dd7e6dd4ee5f4be90efb082adf9870
vuk@ualberta.ca <vuk@ualberta.ca>
2018-04-05 00:18:43 -0600

    working card example for test1

--- packageManager/src/components/testing/test1.vue
+++ packageManager/src/components/testing/test1.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1>Performance</h1>
+	<div>
+  	<h1>Performance</h1>
+  	<junit4> </junit4>
+  </div>
 </template>
 
 <script>
+import junit4 from '@/components/testing/junit4'
+
 export default {
-  name: 'test1'
+  name: 'test1',
+  components: {
+    junit4
+  }
 }
 </script>

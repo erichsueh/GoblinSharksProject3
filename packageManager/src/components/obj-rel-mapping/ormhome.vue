3110e5795c9f0237509869f04c77f14ee5ab56d1
liaowilly12123 <willyliao0425@gmail.com>
2018-04-05 23:08:23 -0600

    finished domain homes

---
+++ packageManager/src/components/obj-rel-mapping/ormhome.vue
@@ -0,0 +1,29 @@
+<template>
+	<center>
+	<div>
+	<br>
+	  <h1>Object Relational Mapping</h1>
+	  <br>
+	  <b-row>
+	  	<b-col cols="2">
+	  	</b-col>
+	  	<b-col cols="">
+	  		<p class="homeTxt"> Object-Relational Mapping is a programming technique used to convert data between incompatible type systems using object-oriented programming languages. It creates a “virtual object database” which can be used within the programming language of choice.</p>
+	  	</b-col>
+	  	<b-col cols="2">
+	  	</b-col>
+	  </b-row>
+	 </div>
+	 </center>
+</template>
+
+<script>
+export default {
+  name: 'test1'
+}
+</script>
+<style>
+.homeTxt {
+	text-align: left;
+}
+</style>
---
+++ packageManager/src/components/security/sechome.vue
@@ -0,0 +1,29 @@
+<template>
+	<center>
+	<div>
+	<br>
+	  <h1>Security</h1>
+	  <br>
+	  <b-row>
+	  	<b-col cols="2">
+	  	</b-col>
+	  	<b-col cols="">
+	  		<p class="homeTxt">Security libraries can help with the protection of your application by protecting it against vulnerabilities and attacks such as: Backdoors, Tampering, Phishing, etc.</p>
+	  	</b-col>
+	  	<b-col cols="2">
+	  	</b-col>
+	  </b-row>
+	 </div>
+	 </center>
+</template>
+
+<script>
+export default {
+  name: 'test1'
+}
+</script>
+<style>
+.homeTxt {
+	text-align: left;
+}
+</style>

769b80a1a7c7d96f1de92ac07a29ca4eb716bde8
vuk@ualberta.ca <vuk@ualberta.ca>
2018-04-05 20:06:16 -0600

    links to github and some nice card styling

--- packageManager/src/components/testing/junit4.vue
+++ packageManager/src/components/testing/junit4.vue
@@ -3,33 +3,33 @@
   <b-card>
     <b-row>
       <b-col>
-
         <a class="card-link" target="_blank" href="https://github.com/junit-team/junit4" id="logo"> 
           <h1 class="card-header">JUnit</h1> 
         </a>
-
         <b-card-body>
           <p>
             JUnit is a simple framework to write repeatable tests. It is an instance of the xUnit architecture for unit testing frameworks.
           </p>
         </b-card-body>
-        <p>Last Modified Date: 2018-02-15</p>
-        <b-button target="_blank" href="https://github.com/junit-team/junit4"> GitHub Link</b-button>
+        <p class="date">Last Modified Date: 2018-02-15</p>
+        <a target="_blank" href="https://github.com/junit-team/junit4">
+          <img src="../../assets/github1.png" class="gitImage">
+        </a>
       </b-col>
       
       <b-col>
-        <img src="../../assets/JUnit.jpg"/>
+        <img src="../../assets/JUnit.jpg" class="graphs">
       </b-col>
 
     </b-row>
   
   </b-card>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'test4'
+  name: 'junit4'
 }
 </script>
 
@@ -43,10 +43,29 @@ img {
   width: 500px;
 }
 
-.gitBtn {
-  width: 100px;
-  height: 100px;
+.card-text {
+  padding-left: 50px;
+  padding-right: 50px;
 }
 
+a:hover {
+  text-decoration: none;
+}
+
+.card-header {
+  background-color: white;
+}
+
+.butsize {
+  height: 40;
+  width: 40;
+}
+.gitImage {
+  width: 175px;
+  float: left;
+}
+.date {
+  padding-left: 22px;
+}
 
 </style>
